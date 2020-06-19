9f9a17a822218ceb9e1a6d5bc63d90e489950f25
Leandro Real <leo.real2585@gmail.com>
2020-06-19 09:00:06 -0300

    v1

---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i>
+            por <strong>Dev <span className="text-danger">R3al</span></strong>
+        </span>
+    </footer>
---
+++ frontend/src/components/template/Header.jsx
@@ -0,0 +1,13 @@
+import './Header.css'
+import React from 'react'
+
+export default props => 
+    <header className="header d-nome d-sm-flex flex-column">
+        <h1 claaName="mt-3">
+            <i className={`fa fa-${props.icon}`}></i> 
+            {props.title}
+        </h1>
+        <p className="lead text-muted">
+            {props.subtitle}
+        </p>
+    </header>
